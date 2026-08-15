63eb64dc5cf73d724457c417b9428815a5f46d3a
Maksim Sergienko <creativesun@yandex.ru>
2026-08-15 18:25:51 +0300

    feat(release): bump version to 0.3.0 and update changelog with new features and changes

--- mrc-exporter/mrc-exporter.lua
+++ mrc-exporter/mrc-exporter.lua
@@ -3,7 +3,7 @@ local ADDON_NAME = ...
 MrcExporter = MrcExporter or {}
 local Addon = MrcExporter
 
-Addon.version = "0.2.0"
+Addon.version = "0.3.0"
 -- Filled from ## X-LastUpdated in mrc-exporter.toc on load.
 Addon.lastUpdated = ""
 
